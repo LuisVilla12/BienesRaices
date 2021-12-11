8bce15efe531b22c5d207168b7f1304ea3d3a356
Luis Villa <luisji2312ia@gmail.com>
2021-12-11 00:24:08 -0600

    v04 Creacion de archivos Admin

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+    require '../includes/funciones.php';
+    incluirTempleate('header');
+?>
+    <main class="contenedor seccion">
+        <h1>Bienvenido</h1>
+        <a href="/admin/propiedades/crear.php" class="boton boton-verde">Crear propiedad</a>
+        
+    </main>
+
+<?php incluirTempleate('footer'); ?>
---
+++ admin/propiedades/crear.php
@@ -0,0 +1,10 @@
+<?php
+    require '../../includes/funciones.php';
+    incluirTempleate('header');
+?>
+    <main class="contenedor seccion">
+        <h1>Crear</h1>
+        <a href="/admin" class="boton boton-verde">Volver</a>
+    </main>
+
+<?php incluirTempleate('footer'); ?>
